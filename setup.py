e879c0f7b3fcfc047828f9d3c7e3e0130bb9d285
ponkster <ponkster@orgasponk.(none)>
2013-05-07 14:22:44 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'pyramid_debugtoolbar',
+    'waitress',
+    ]
+
+setup(name='PyWarehouse',
+      version='0.0',
+      description='PyWarehouse',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Imron Setio Widodo',
+      author_email='origin.ponkster@gmail.com',
+      url='',
+      keywords='simple warehouse',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="pywarehouse",
+      entry_points="""\
+      [paste.app_factory]
+      main = pywarehouse:main
+      """,
+      )
